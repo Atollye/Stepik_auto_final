3dad334076717368f20e9b97702dc9d8e0732a39
Alayna19 <kuznetsova.a23@gmail.com>
2023-07-26 19:04:39 +0300

    add test_guest_cant_see_product_in_basket_opened_from_product_page

--- ui_tests/test_product_page.py
+++ ui_tests/test_product_page.py
@@ -8,24 +8,35 @@ from pages.basket_page import BasketPage
 
 
 
-sample_product_url = (
+SAMPLE_PRODUCT_URL = (
     "http://selenium1py.pythonanywhere.com/catalogue/reversing_202/"
 )
 
 class TestProductPageHeader():
 
     def test_guest_should_see_login_link_on_product_page(self, browser):
-        page = ProductPage(browser, sample_product_url)
+        page = ProductPage(browser, SAMPLE_PRODUCT_URL)
         page.open()
         page.should_be_login_link()
     
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
-        page = ProductPage(browser, sample_product_url)
+        page = ProductPage(browser, SAMPLE_PRODUCT_URL)
         page.open()
         page.should_be_login_link()
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
+
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(
+        self, browser
+        ):
+        page = ProductPage(browser, SAMPLE_PRODUCT_URL)
+        page.open()
+        page.should_be_link_to_basket()
+        page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_no_items_in_basket()
+        basket_page.should_be_basket_emty_text()
 
 
 class TestSpecialOffers():
